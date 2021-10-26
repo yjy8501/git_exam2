63c65ef1ecadff40655fd5ec93a75e8e9bb5f443
Yang, Jaeyong <yjy850146@gmail.com>
2021-10-26 09:53:21 +0900

    201734017-5

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,9 +3,10 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201734017");
-		System.out.println("양재용");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201734017");
+		System.out.println("이름: 양재용");
 	}
 
 }
+
